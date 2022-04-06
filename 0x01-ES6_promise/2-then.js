954258b4f96e86c65a0896caadcb36e0fa1266d8
aloicerains <aloiceokoth98@gmail.com>
2022-04-06 22:02:34 +0300

    version

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,5 @@
-export default function handleResponseFromAPI (promise) {
+export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-    return ({
-      status: 200,
-      body: 'success'
-    })
-  })
-    .catch((err) => {
-      return err
-    })
-    .then(() => {
-      console.log('Got a response from the API')
-    })
+    console.log('Got a response from the API');
+  }, (err) => err);
 }
