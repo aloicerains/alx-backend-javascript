e4d83d53a12ff4a657f85fd068381daba993093b
aloicerains <aloiceokoth98@gmail.com>
2022-04-06 21:57:33 +0300

    taks adjusted

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,14 @@
 export default function handleResponseFromAPI (promise) {
   promise.then(() => {
-    console.log('Got a response from the API')
     return ({
       status: 200,
       body: 'success'
     })
   })
     .catch((err) => {
-      console.log('Got a response from the API')
       return err
+    })
+    .then(() => {
+      console.log('Got a response from the API')
     })
 }
