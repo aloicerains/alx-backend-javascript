b44d390730454a0949957ff7481741a3212949ca
aloicerains <aloiceokoth98@gmail.com>
2022-04-07 11:06:15 +0300

    lint errors

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
-export default function getFullResponseFromAPI (success) {
+export default function getFullResponseFromAPI(success) {
   const promise = new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success'
-      })
+        body: 'Success',
+      });
     } else reject(Error('The fake API is not working currently'))
-  })
-  return promise
-};
+  });
+  return promise;
+}
