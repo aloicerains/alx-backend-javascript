ad6e007ee98be547646cac40e0a68e80dc6c39e2
aloicerains <aloiceokoth98@gmail.com>
2022-06-01 00:38:47 +0300

    edits

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,23 +4,17 @@ const countStudents = (path) => {
   if (fs.existsSync(path) === false) {
     throw new Error('Cannot laod the database');
   }
-  const { load } = require('csv-load-sync');
-  const csv = load(path);
-  console.log(`Number of students: ${csv.length}`);
-  // console.log(csv);
-  const fields = {};
-  let count = 0;
-  csv.forEach((record) => {
-    if (record.field in fields) {
-      let val = record.firstname;
-      if (count < csv.length - 2) { val = ` ${val}`; }
-      count++;
-      fields[record.field].names.push(val);
-      fields[record.field].count++;
-    } else { fields[record.field] = { names: [record.firstname], count: 1 }; }
-  });
-  Object.entries(fields).forEach(([key, value]) => {
-    console.log(`Number of students in ${key}: ${value.count}. List: ${value.names}`);
-  });
+  const data = fs.readFileSync(path, 'utf8');
+  const students = data.split('\n')
+    .map((student) => student.split(','))
+    .filter((student) => student.length === 4 && student[0] !== 'firstname')
+    .map((student) => ({ firstName: student[0], field: student[3] }));
+  const cs = students.filter((student) => student.field === 'CS')
+    .map((student) => student.firstName);
+  const swe = students.filter((student) => student.field === 'SWE')
+    .map((student) => student.firstName);
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in CS: ${swe.length}. List: ${swe.join(', ')}`);
 };
 module.exports = countStudents;
