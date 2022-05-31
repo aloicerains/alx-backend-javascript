a31683d5ddf54843d5b92279b30fe2490f9a4953
aloicerains <aloiceokoth98@gmail.com>
2022-05-31 21:53:10 +0300

    task one

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+// Program to be executed through the command line
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
